bc91a193090b696492f6fb319d4b5149c24ae2cb
SusiIrwin <susi.irwin@gmail.com>
2016-07-17 21:47:40 -0600

    added play text

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -15,7 +15,11 @@ class Battleship
   end
 
   def play
-    "enter ship layout"
+  "  I have laid out my ships on the grid.
+  You now need to layout your two ships.
+  The first is two units long and the
+  second is three units long.
+  The grid has A1 at the top left and D4 at the bottom right."
   end
 
 end

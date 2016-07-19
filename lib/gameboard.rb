8267e6eedf4f9813ce6d04142d18121bc7449e55
SusiIrwin <susi.irwin@gmail.com>
2016-07-19 14:48:31 -0600

    I have a working gameboard that takes misses and validates shots

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -1,52 +1,43 @@
-class Gameboard
-  attr_reader :positions,
-              :coordinate_for_two_ship,
-              :coordinate_for_three_ship
+class GameBoard
+  attr_reader :gameboard
 
-      ROWS = [1,2,3,4]
-      COLUMNS = ["A", "B", "C", "D"]
-
-  def initialize
-    @positions = [["A1", "A2", "A3", "A4"],
-                  ["B1", "B2", "B3", "B4"],
-                  ["C1", "C2", "C3", "C4"],
-                  ["D1", "D2", "D3", "D4"]]
-
-    # valdation ideas:
-    #grab index of array and call that index in next row
-    #increase in row by 1 - that is the first position over
-    #game boundaries based on numbers of rows and number of columns
-  end
-
-  def get_random_start_position
-    starting_row = (ROWS.count * rand).to_i
-    starting_column = (COLUMNS.count * rand).to_i
-
-    @coordinates = [[starting_row],[starting_column]] # => [[0],[3]]
-    #not sure the above line does what i need -
-    #convert the positions to the space in the array name
-    # so [[3],[3]] becomes "D4"; [[1], [2]] becomes B3
-    flattened_coordinates = @coordinates.flatten # => [0,3]
-    flattened_coordinates =
-    @positions.fetch(@coordinates)
-
-    #pick a point, find the directions to go in to follow rulesa
-    #if my point is A2 i can either go one spot left, one spot right or one spot down. I can
-    # increase my array position by 1 and get to A3. I can decrease my position by 1 and go to A1.
-    # I can somehow get to B2 - and that would be a different array but the same position within the array.
-
-
-
-    # @coordinate_for_two_ship = positions.sample(2).sort
-    # @coordinate_for_two_ship
-    # coordinates = [position.sample]
-    # if down
-    #   coordinates << add_one_to_start(coordinates[0])
+  def initialize(size = 4)
+    @size = size
+    @gameboard = generate_gameboard
+    @stored_shots = []
 
   end
 
-  def get_random_three_positions
-    @coordinate_for_three_ship = positions.sample(3).sort
-    @coordinate_for_three_ship
+  def generate_line
+    @size.times.map do
+      " "
+    end
   end
+
+  def generate_gameboard
+    @size.times.map do
+      generate_line
+    end
+  end
+
+  def record_shot(y,x)
+    if valid_shot?(y,x)
+      mark_shot_on_board(y,x)
+    else
+      "You have already hit this spot!"
+    end
+  end
+
+  def mark_shot_on_board(y,x)
+    @gameboard[y][x] = "M"
+    @stored_shots << [y,x]
+    [y,x]
+  end
+
+  def valid_shot?(y,x)
+    @stored_shots.none? do |coordinate|
+      coordinate[0] == y && coordinate[1] == x
+    end
+  end
+
 end
--- test/gameboard_test.rb
+++ test/gameboard_test.rb
@@ -3,13 +3,95 @@ require './lib/gameboard'
 
 class GameboardTest < Minitest::Test
 
-  def test_it_has_positions
-    board = Gameboard.new
-    expected = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4"]
+  def test_it_creates_a_new_board
+    board = GameBoard.new
 
-    assert_equal expected, board.positions
+    assert_instance_of GameBoard, board
   end
 
-  
+  def test_it_assembles_2_x_2_board
+    board = GameBoard.new(2)
 
+    assert_equal 2, board.gameboard.length
+  end
+
+  def test_it_assembles_3_x_3_board
+    board = GameBoard.new(3)
+
+    assert_equal 3, board.gameboard.length
+  end
+
+  def test_it_generates_a_line
+    board = GameBoard.new
+
+    assert_equal [" ", " ", " ", " "], board.generate_line
+  end
+
+  def test_it_generates_complete_gameboard
+    board = GameBoard.new
+
+    assert_equal [[" ", " ", " ", " "],
+                  [" ", " ", " ", " "],
+                  [" ", " ", " ", " "],
+                  [" ", " ", " ", " "]], board.generate_gameboard
+  end
+
+  def test_it_can_record_a_shot_on_a_small_board
+    board = GameBoard.new
+
+    assert_respond_to board, :record_shot
+  end
+
+  def test_we_can_pass_coordinates_into_record_shot
+    board = GameBoard.new
+
+    assert_equal [0,0], board.record_shot(0,0)
+  end
+
+  def test_we_can_pass_different_coordinates_into_record_shot
+    board = GameBoard.new
+
+    assert_equal [1,1], board.record_shot(1,1)
+  end
+
+  def test_it_places_shot_on_2_x_2_board
+    board = GameBoard.new(2)
+    board.record_shot(0,0)
+
+    assert_equal [["M", " "],
+                  [" ", " "]], board.gameboard
+  end
+
+  def test_it_can_place_two_shots_on_a_board
+    board = GameBoard.new
+    board.record_shot(1,3)
+    board.record_shot(3,2)
+
+    assert_equal [[" ", " ", " ", " "],
+                  [" ", " ", " ", "M"],
+                  [" ", " ", " ", " "],
+                  [" ", " ", "M", " "]], board.gameboard
+  end
+
+  def test_it_validates_shot
+    board = GameBoard.new
+    board.record_shot(3,2)
+
+    assert_equal false, board.valid_shot?(3,2)
+    assert_equal true, board.valid_shot?(2,3)
+  end
+
+  def test_it_cannot_take_a_shot_in_the_same_place_twice
+    board = GameBoard.new
+    board.record_shot(3,2)
+
+    assert_equal "You have already hit this spot!", board.record_shot(3,2)
+  end
+
+  def test_it_marks_shot_on_board
+    board = GameBoard.new
+    board.record_shot(2,2)
+
+    assert_equal [2,2], board.mark_shot_on_board(2,2)
+  end
 end
